f0a3f64d8edea980f60826a0bc7a710abc7afb6b
saurabh3460 <saurabh3460@gmail.com>
2020-09-28 10:11:38 +0530

    Store Remote State

---
+++ codes/Store Remote State/main.tf
@@ -0,0 +1,29 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+terraform {
+  backend "remote" {
+    organization = "CodeForFun"
+
+    workspaces {
+      name = "remote-state-test"
+    }
+  }
+
+}
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-086c142842468ba9d"
+  instance_type = "t2.micro"
+}
